22331c0a2dfcc9a038a7cfc40766e7f31f0a13b5
Bedknobs <noeliareysilva@gmail.com>
2024-04-07 11:51:57 +0200

    Añadido estilo al slider

--- frontend/src/Components/Slider.jsx
+++ frontend/src/Components/Slider.jsx
@@ -1,20 +1,54 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
+import "./Slider.css";
 import { Navigation } from "swiper/modules";
+
 export default function Slider() {
   return (
     <div>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+      <Swiper
+        slidesPerView={5}
+        navigation={true}
+        modules={[Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="../../src/assets/platosCombinados.jpg" alt="" />
+          <p>TEXTO</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../../src/assets/platosCombinados.jpg" alt="" />
+          <p>TEXTO</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../../src/assets/platosCombinados.jpg" alt="" />
+          <p>TEXTO</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../../src/assets/platosCombinados.jpg" alt="" />
+          <p>TEXTO</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../../src/assets/platosCombinados.jpg" alt="" />
+          <p>TEXTO</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../../src/assets/platosCombinados.jpg" alt="" />
+          <p>TEXTO</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../../src/assets/platosCombinados.jpg" alt="" />
+          <p>TEXTO</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../../src/assets/platosCombinados.jpg" alt="" />
+          <p>TEXTO</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../../src/assets/platosCombinados.jpg" alt="" />
+          <p>TEXTO</p>
+        </SwiperSlide>
       </Swiper>
     </div>
   );
