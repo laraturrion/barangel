eedb34dead00f106f3e340f2a3a3d5ae1ab52b36
Bedknobs <noeliareysilva@gmail.com>
2024-04-11 19:13:47 +0200

    Fixed route problem

---
+++ frontend/src/Contacto.jsx
@@ -0,0 +1,6 @@
+
+export default function Contacto() {
+  return (
+    <div>Contacto</div>
+  )
+}
---
+++ frontend/src/Eventos.jsx
@@ -0,0 +1,6 @@
+
+export default function Eventos() {
+  return (
+    <div>Eventos</div>
+  )
+}
