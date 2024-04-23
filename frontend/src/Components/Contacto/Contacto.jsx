399c9d503bb30a6c44bfc6e9a394f409cb9df937
laraturrion <laturrions@gmail.com>
2024-04-23 19:27:04 +0200

    cambios css contacto

--- frontend/src/Components/Contacto/Contacto.jsx
+++ frontend/src/Components/Contacto/Contacto.jsx
@@ -16,27 +16,15 @@ export default function Contacto() {
           contactarnos a través de cualquiera de los métodos a continuación:
         </p>
         <p className="visitanosContacto">
-          Visítanos Ven a disfrutar de una experiencia gastronómica única en
-          nuestro restaurante. Estamos ubicados en Av. Castilla, 18, 33550
-          Cangas de Onis, Asturias. Nuestro acogedor ambiente y nuestro
-          delicioso menú te esperan.
+          Estamos ubicados en Av. Castilla, 18, 33550 Cangas de Onis, Asturias.
+          Nuestro acogedor ambiente y nuestro delicioso menú te esperan.
         </p>
         <p className="telefonoContacto">
-          Llámanos Si prefieres hablar directamente con nosotros, no dudes en
-          llamarnos al [número de teléfono]. Estamos disponibles durante horas
-          de operación para atender tus llamadas y responder tus preguntas.
-        </p>
-
-        <p className="redesContacto">
-          Síguenos en Redes Sociales Mantente al día con nuestras últimas
-          novedades, promociones y eventos siguiéndonos en nuestras redes
-          sociales: Facebook: [enlace a la página de Facebook] Instagram:
-          [enlace a la cuenta de Instagram]
+          No dudes en llamarnos 984 51 71 16 para reservar
         </p>
 
         <p className="horario">
-          Horario de Atención Estamos abiertos [días de la semana] de [horario
-          de apertura] a [horario de cierre]. ¡Esperamos verte pronto!
+           Abrimos todos los días de 11:00 a 00:00, miercoles cerrado por descanso.
         </p>
       </div>
       <Footer />
