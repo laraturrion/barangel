e673ea7a074401fedc93c9853d5607b2b23abde6
laraturrion <laturrions@gmail.com>
2024-04-25 19:32:10 +0200

    enlace footer google maps

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -1,11 +1,15 @@
 import "./footer.css";
 
+ 
 export const Footer = () => {
+
+  const enlaceGoogleMaps= 'https://www.google.es/maps/place/Bar+Restaurante+Angel/@43.3482069,-5.1337541,17z/data=!4m7!3m6!1s0xd36216edcd5f09b:0x7138c354bbee3b0a!8m2!3d43.3482069!4d-5.1311792!10e2!16s%2Fg%2F11txpctkj7?entry=ttu'
   return (
     <>
       <section className="Footer">
         <article className="footerIzquierdo">
           <div className="localizacion">
+          <a href={enlaceGoogleMaps} target="_blank" rel="noopener noreferrer" style={{ color: 'inherit',textDecoration: 'none' }}>
             <img
               src="../../src/assets/LogoMapa.svg"
               alt="Logo Mapa"
@@ -13,6 +17,7 @@ export const Footer = () => {
             />
             <span>Av. Castilla, 18, 33550 Cangas de Onis, Asturias</span>{" "}
             {/* Texto junto al logo del mapa */}
+            </a>
           </div>
           <div className="telefonoContacto">
             <img
